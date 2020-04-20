9dd50374225b99e2d8b7e27f35f5e764fba9fe12
issachareli <59080214+issachareli@users.noreply.github.com>
2020-04-21 01:17:45 +0900

    Update Tabs.js

--- src/views/elements/Tabs.js
+++ src/views/elements/Tabs.js
@@ -50,7 +50,7 @@ class TabsPage extends Component {
           <TabPane tabId="1">
             <Row>
               <Col sm="12">
-              <div className="m-b"><h4>How to use your MoneyLiFE Sponsor’s Portal (4 Lessons)</h4></div>
+              <div className="mb-4 m-t"><h4>How to use your MoneyLiFE Sponsor’s Portal (4 Lessons)</h4></div>
               </Col>
         <Col md={4}>
       <a href="https://www.youtube.com/watch?v=FQYl0o12fxY&t=8s" target="_blank" class="custom-card">
