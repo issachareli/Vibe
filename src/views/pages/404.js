dd16df2c5bbf106ee1eef587046f91d491171679
issachareli <59080214+issachareli@users.noreply.github.com>
2020-04-22 03:47:23 +0900

    Update 404.js

--- src/views/pages/404.js
+++ src/views/pages/404.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ModalVideo from 'react-modal-video'
 
-class App extends React.Component {
+class ErrorPage extends React.Component {
 
   constructor () {
     super()
