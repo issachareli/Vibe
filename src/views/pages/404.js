5985f9755386f24c36b6ccb5eb2b3e43d4b22db0
issachareli <59080214+issachareli@users.noreply.github.com>
2020-04-22 03:46:42 +0900

    Update 404.js

--- src/views/pages/404.js
+++ src/views/pages/404.js
@@ -1,16 +1,34 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ModalVideo from 'react-modal-video'
 
-const ErrorPage = () => {
-  return (
-    <div>
-      <div className="m-t-xxl text-center">
-        <h1 className="error-number">404</h1>
-        <h3 className="m-b">Sorry but we couldnt find this page. It doesn't exist!</h3>
-        <NavLink to={'/home'}>Go Home!</NavLink>
+class App extends React.Component {
+
+  constructor () {
+    super()
+    this.state = {
+      isOpen: false
+    }
+    this.openModal = this.openModal.bind(this)
+  }
+
+  openModal () {
+    this.setState({isOpen: true})
+  }
+
+  render () {
+    return (
+      <div>
+        <ModalVideo channel='youtube' isOpen={this.state.isOpen} videoId='L61p2uyiMSo' onClose={() => this.setState({isOpen: false})} />
+        <button onClick={this.openModal}>Open</button>
       </div>
-    </div>
-  );
-};
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+    document.getElementById('root')
+)
 
 export default ErrorPage;
