b42a86405c59dcaa5c7f67fb15a904a4b0fbe311
issachareli <59080214+issachareli@users.noreply.github.com>
2020-04-21 16:12:26 +0900

    Update Modals.js

--- src/views/elements/Modals.js
+++ src/views/elements/Modals.js
@@ -24,7 +24,7 @@ class ModalsPage extends Component {
                     <Modal isOpen={this.state.modal} toggle={this.toggle}>
                         <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
                         <ModalBody>
-                            <div class="embed-responsive embed-responsive-16by9">
+                            <div class="embeded embed-responsive-16by9">
   <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
 </div>
                         </ModalBody>
